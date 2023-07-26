a1c6e869cccb7a5bac4aa568194f10b4d040ef33
debize <lucas.debize@epitech.eu>
2023-07-27 01:38:49 +0200

    ADD first push

---
+++ backend/main.js
@@ -0,0 +1,9 @@
+const axios = require('axios');
+const API_KEY = 'pat-na1-e9a27d01-43f8-4e0c-b158-192a5d0cf71c';
+const BASE_URL = 'https://api.hubapi.com/';
+
+async function main() {
+
+}
+
+main();
